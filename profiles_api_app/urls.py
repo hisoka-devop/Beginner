f83a4b7a5463aeca3609dc29231f6fd04c12155f
hisoka-devop <hisoka.devop@gmail.com>
2019-10-18 16:21:46 +0300

    Finsihed viewsets

--- profiles_api_app/urls.py
+++ profiles_api_app/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from profiles_api_app import views
+from rest_framework.routers import DefaultRouter
+
+#definig a router
+router = DefaultRouter()
+router.register('HelloViewSet/', views.HelloViewSet, base_name='HelloViewSet')
 
 urlpatterns = [
-                path('HelloApiView/', views.HelloApiView.as_view())
+                path('HelloApiView/', views.HelloApiView.as_view()),
+                path('', include(router.urls)),
 ]
--- profiles_api_app/views.py
+++ profiles_api_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework import viewsets
 from rest_framework.response import Response
 
 from rest_framework import status
 from profiles_api_app import serializers
 
 # Create your views here.
+
+
 class HelloApiView(APIView):
     """
     A class of the APIView to return simple hello world and message
@@ -26,7 +29,7 @@ class HelloApiView(APIView):
             'Uses HTTP Methods',
             'Similar to traditional http websites',
             'Gives you Control over Application'
-            ]
+        ]
 
         return Response({
             'message': 'Hello to the API World',
@@ -49,14 +52,14 @@ class HelloApiView(APIView):
             })
         else:
             return Response(serializer.errors,
-                            status = status.HTTP_400_BAD_REQUEST,
+                            status=status.HTTP_400_BAD_REQUEST,
                             )
 
     def put(self, request, pk=None):
         '''To update full objects'''
         return Response(
             {
-                'Method':'PUT',
+                'Method': 'PUT',
             }
         )
 
@@ -64,7 +67,7 @@ class HelloApiView(APIView):
         '''To udpate objects partially'''
         return Response(
             {
-                'Method':'PATCH',
+                'Method': 'PATCH',
             }
         )
 
@@ -72,6 +75,59 @@ class HelloApiView(APIView):
         '''To delete an object fully'''
         return Response(
             {
-                'Method':'DELETE',
+                'Method': 'DELETE',
             }
         )
+
+
+class HelloViewSet(viewsets.ViewSet):
+    '''A class to handle the the viewsets'''
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        '''A simple implementation of an easy list'''
+        a_viewset = [
+            'User Actions: list, create, retrieve, update, partial_update',
+            'It uses Routers rather than normal urlpatterns.path',
+            'Provides more functionality with less code'
+        ]
+
+        return Response({
+            'msg': 'Greetings',
+            'a_viewset': a_viewset
+        })
+
+    def create(self, request):
+        '''A simple implementation of creating a new object, POST alternative'''
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            return Response({
+                'msg': f'Hello {name}',
+            })
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST,)
+
+    def retrieve(self, request, pk=None):
+        '''A simple implementation of get, getting a specified object'''
+        return Response({
+            'Method': 'GET',
+        })
+
+    def update(self, request, pk=None):
+        '''A simple implementation of update, which is the replacement of PUT'''
+        return Response({
+            'Method': 'PUT',
+        })
+
+    def partial_update(self, request, pk=None):
+        '''A simple implementation of partial_update, which is the replacement of PATCH'''
+        return Response({
+            'Method': 'PATCH',
+        })
+
+    def destroy(self, request, pk=None):
+        '''A simple implementation of destroy, which is the replacement of the delete'''
+        return Response({
+            'Method':'DELETE'
+        })
